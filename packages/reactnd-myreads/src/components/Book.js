2eb06d752565f108c3b905a7b3d1991c99f87dc7
Carlos Loureda <balanzeneto@gmail.com>
2019-03-26 21:00:02 +0100

    [my-reads] Created Book and BookShelf components with fake data

---
+++ packages/reactnd-myreads/src/components/Book.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Book extends Component {
+  static propTypes = {
+    /* The book object */
+    book: PropTypes.object.isRequired
+  };
+
+  render() {
+    const { book } = this.props;
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url("${book.imageLinks.smallThumbnail}")`
+            }}
+          />
+          <div className="book-shelf-changer">
+            <select>
+              <option value="move" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        {book.authors.map(author => (
+          <div key={author} className="book-authors">
+            {author}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
---
+++ packages/reactnd-myreads/src/components/BookShelf.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Book from "./Book";
+
+export default class BookShelf extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    shelfName: PropTypes.string.isRequired
+  };
+
+  render() {
+    const { books, shelfName } = this.props;
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfName}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map(book => (
+              <li key={book.title}>
+                <Book book={book} />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
