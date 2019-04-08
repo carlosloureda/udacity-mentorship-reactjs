46e2be08bbf41be33962b208545c8c3be9630ad4
Carlos Loureda <balanzeneto@gmail.com>
2019-04-08 04:13:25 +0200

    Redux. Adding Goals to TODO app example

--- packages/react-redux-udacity/1.create_store.js
+++ packages/react-redux-udacity/1.create_store.js
@@ -53,6 +53,23 @@ function todos(state = [], action) {
   }
 }
 
+function goals(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE_GOAL":
+      return state.filter(goal => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  };
+}
+
 const store = createStore(todos);
 const unsubscribe = store.subscribe(() => {
   console.log("The new state is: ", store.getState());
